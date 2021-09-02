55ec224cc2665c9ec7c8e3bb81cc5c0ff39d11f2
haduigon <haduigon@gmail.com>
2021-09-02 09:31:38 +0000

    First commit

---
+++ request.php
@@ -0,0 +1,24 @@
+<?php
+
+if(isset($_POST['userEmail'])){
+
+
+$email=($_POST['userEmail']);
+//$email2=$email->nameUser;
+//$email3=json_encode($email);
+echo($email);
+return $_POST['userEmail'];
+//echo $email2;
+
+
+}else{
+
+echo "[eq";
+}
+
+
+
+
+
+
+?>
